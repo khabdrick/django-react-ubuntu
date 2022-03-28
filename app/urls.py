f08b11725aa272c9ef84660d333ca2cfcde70a8f
Muhammed Ali <muhamzyali@gmail.com>
2022-03-28 16:43:10 +0100

    update url

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.api_endpoints, name='home')
+    path('', views.api_endpoints, name='home'),
+    path('create/', views.create_contact, name='add-items'),
+    
 ]
