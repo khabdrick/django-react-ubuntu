01c45ead60d4d998b728406e9d2551fa7b3e71f7
Muhammed Ali <muhamzyali@gmail.com>
2022-03-28 19:51:58 +0100

    update the url

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.api_endpoints, name='home'),
     path('create/', views.create_contact, name='create-items'),
-    path('list/', views.view_contacts, name='view-contacts'),
+    path('list/', views.UserList.as_view(), name='view-contacts'),
     path('update/<int:pk>/', views.update_contacts, name='update-contacts'),
-    path('item/<int:pk>/delete/', views.delete_contact, name='delete-contact'),
+    path('<int:pk>/delete/', views.delete_contact, name='delete-contact'),
 
 ]
