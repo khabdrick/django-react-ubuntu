ffb76773075e61a42845762e7c3a6a4976d262be
Muhammed Ali <muhamzyali@gmail.com>
2022-03-28 19:51:38 +0100

    corrected serialization

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,4 @@ from .models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ('name', 'phone_name')
+        fields = ('name', 'phone_number')
