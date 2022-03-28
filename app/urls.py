6f829bfd1906e02ccdef58df930f4f4eb92c7a60
Muhammed Ali <muhamzyali@gmail.com>
2022-03-28 18:27:16 +0100

    delete url

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create/', views.create_contact, name='create-items'),
     path('list/', views.view_contacts, name='view-contacts'),
     path('update/<int:pk>/', views.update_contacts, name='update-contacts'),
-
+    path('item/<int:pk>/delete/', views.delete_contact, name='delete-contact'),
 
 ]
