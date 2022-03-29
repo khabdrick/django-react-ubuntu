0bf692770d44d35c71c4f6e1f38f08ea522ccf87
Muhammed Ali <muhamzyali@gmail.com>
2022-03-29 20:03:56 +0100

    card view

---
+++ frontend/src/components/card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import { ContactPage } from "./components/ContactList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Detail } from "./components/detailView";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              < ContactPage />
+            </Route>
+            <Route path="/:id">
+              <Detail />
+            </Route>
+          </Switch>
+        </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
