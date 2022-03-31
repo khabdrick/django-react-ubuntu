257ee0413d33f257ad8b302d616451384ff9b9d3
Muhammed Ali <muhamzyali@gmail.com>
2022-03-31 20:21:38 +0100

    add detail to serializer

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,4 @@ from .models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ('name', 'phone_number')
+        fields = ('name', 'phone_number',"id")
