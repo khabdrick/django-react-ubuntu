4020072ecbe572f039e9557b7ba45527ff6e7236
Muhammed Ali <muhamzyali@gmail.com>
2022-04-01 07:59:47 +0100

    complete create form

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,21 +1,46 @@
-import React from "react"
+import React,{useState} from "react"
+import axios from 'axios';
+
+export const Form = () => {
+
+    const [name1, setName1] = useState('')
+    const [phone_number, setPhone_number] = useState('') 
+    
+    let formField = new FormData()
+    formField.append('name',name1)
+    formField.append('phone_number',phone_number)
 
 
-export const Form = ({input, onFormChange, onFormSubmit}) => {
-
-    const handleChange = (event)=>{
-        onFormChange(event.target.value)
-    }
-
-    const handleSubmit = (event)=>{
-        event.preventDefault()
-        onFormSubmit()
-    }
+    // const handleSubmit = (event)=>{
+    //     event.preventDefault()
+    //     onFormSubmit()
+    // }
+    const handleFormSubmit = async () => {
+        
+        
+        await axios({
+            method: 'post',
+            url:'/app/create/',
+            data: formField
+          }).then(response=>{
+            console.log(response.data);
+            // history.push('/')
+          })
+          .then((response) => console.log(response))
+          .then((data) => {
+            console.log("dddddddd",data);
+            // getUpdate();
+          });
+          
+      };
+      
+      
 
     return(
         <>
-        <form onSubmit={handleSubmit}>
-            <input className='form-class' type='text' required value={input} onChange={handleChange}></input>
+        <form onSubmit={handleFormSubmit}>
+            <input className='form-class' type='text' required value={name1} onChange={(e) => setName1(e.target.value)}></input>
+            <input className='form-class' type='text' required value={phone_number} onChange={(e) => setPhone_number(e.target.value)}></input>
             <input  type='submit'></input>
         </form>
         <br/>
